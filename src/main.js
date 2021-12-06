0359965ad944a10eec8562db67680809ef7362c7
Ercan Uzunsakal <ercan@zeo.org>
2021-12-06 21:41:24 +0300

    install v-calendar

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
 import './plugins/bootstrap-vue'
+import VCalendar from 'v-calendar';
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+
+Vue.use(VCalendar, {
+  componentPrefix: 'vc'
+});
 
 new Vue({
   router,
