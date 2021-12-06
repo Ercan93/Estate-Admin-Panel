f73459457626e5cc7f1fd55970e869832af6c9a0
Ercan Uzunsakal <ercan@zeo.org>
2021-12-06 20:25:12 +0300

    add Sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-button class="m-4" v-b-toggle.sidebar-1>Menü</b-button>
+    <b-sidebar id="sidebar-1" title="Iceberg Estates" :no-header-close="false" :visible="true">
+      <div class="px-3 py-2">
+        <div>
+            <b-nav vertical>
+                <b-nav-item>
+                    <router-link to="/CreateAppointment">
+                        <b-button class="btn btn-success"> + Ranevu Oluştur</b-button>
+                    </router-link>
+                </b-nav-item>
+                <b-nav-item>
+                    <router-link to="/">
+                        <b-button class="btn">Ana Sayfa</b-button>
+                    </router-link>
+                </b-nav-item>
+                <b-nav-item>
+                    <b-button class="btn">Randevular</b-button>
+                </b-nav-item>
+            </b-nav>
+        </div>
+      </div>
+    </b-sidebar>
+  </div>
+</template>
+<script>
+export default {
+    name: 'sidebar'
+}
+</script>
