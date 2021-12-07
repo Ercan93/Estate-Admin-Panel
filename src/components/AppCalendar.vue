9d52dc03d9026617be5b4b4de8ddc6f1e0ababac
Ercan Uzunsakal <ercan@zeo.org>
2021-12-07 18:05:48 +0300

    Create AppCalendar.vue

---
+++ src/components/AppCalendar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="my-4">
+    <p>Select Date and Time:</p>
+    <vc-date-picker v-model="date" mode="dateTime" is24hr>
+      <template v-slot="{ inputValue, inputEvents }">
+        <input
+          class="px-2 py-1 border rounded focus:outline-none focus:border-blue-300"
+          :value="inputValue"
+          v-on="inputEvents"
+          required
+        />
+      </template>
+    </vc-date-picker>
+  </div>
+</template>
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+  methods: {
+    ...mapActions(["setAppointmentDate"]),
+  },
+  watch: {
+    date(newDate) {
+      let date = `${newDate.getDate()}-${newDate.getMonth()}-${newDate.getFullYear()}`;
+      let time = `${newDate.getHours()}:${newDate.getMinutes()}`
+      this.setAppointmentDate({ date, time });
+    },
+  },
+};
+</script>
