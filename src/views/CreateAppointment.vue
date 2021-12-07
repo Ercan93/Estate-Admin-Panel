10e602bcb42f4bdce19165de8582a9c014e059f9
Ercan Uzunsakal <ercan@zeo.org>
2021-12-08 00:19:59 +0300

    add AppointmentForm to CreateAppointment

--- src/views/CreateAppointment.vue
+++ src/views/CreateAppointment.vue
@@ -2,106 +2,67 @@
   <div class="container-fluid d-flex flex-column align-items-center">
     <p class="col-6 display-4 mt-4">Create New Appointment</p>
     <div class="col-9">
-      <b-form @submit.prevent="onSubmit" @reset="onReset">
-        <b-form-group
-          id="input-group-1"
-          label="Client Name Surname:"
-          label-for="input-1"
-        >
-          <b-form-input
-            id="input-1"
-            v-model="form.name"
-            placeholder="Bruce Wayne"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-2" label="Email:" label-for="input-2">
-          <b-form-input
-            id="input-2"
-            v-model="form.email"
-            type="email"
-            placeholder="bruce@wayne.com"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-3" label="Employees:" label-for="input-3">
-          <b-form-select
-            id="input-3"
-            v-model="form.employee"
-            :options="employees"
-            required
-          ></b-form-select>
-        </b-form-group>
-
-        <b-form-group id="input-group-4" label="Phone:" label-for="input-4">
-          <b-form-input
-            id="input-4"
-            v-model="form.phone"
-            type="number"
-            placeholder="7351857301"
-            required
-          ></b-form-input>
-        </b-form-group>
-
-        <AppCalendar />
-
-        <GoogleMap />
-
-        <b-button type="submit" class="mr-4 mb-4" variant="success"
-          >Save</b-button
-        >
-        <b-button type="reset" class="mb-4" variant="outline-danger"
-          >Clean Form</b-button
-        >
-      </b-form>
+      <appointment-form :onSubmit="onSubmit" :onReset="onReset" :formDefaults="form">
+        <template v-slot:submitButton>
+          <b-button type="submit" class="mr-4 mb-4" variant="success"
+            >Save</b-button
+          >
+        </template>
+        <template v-slot:resetButton>
+          <b-button type="reset" class="mb-4" variant="outline-danger"
+            >Clean Form</b-button
+          >
+        </template>
+      </appointment-form>
     </div>
   </div>
 </template>
 <script>
-import GoogleMap from "@/components/maps/GoogleMap";
-import AppCalendar from "@/components/AppCalendar";
-import { mapGetters } from 'vuex'
+import AppointmentForm from '@/components/AppointmentForm'
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
       form: {
-        email: "",
         name: "",
+        email: "",
         phone: null,
-        employee: null,
+        employee: '',
+        employees: ["Jason D.", "Micheal F.", "Anna B.", "Tommy K."],
+        addressCode:'',
+        dateTime:{
+          date:'',
+          time:'',
+        }
       },
-      employees: ["Jason D.", "Micheal F.", "Anna B.", "Tommy K."],
     };
   },
   components: {
-    GoogleMap,
-    AppCalendar,
+    AppointmentForm,
   },
   computed: {
-    ...mapGetters(['appointmentGetter'])
+    ...mapGetters(["appointmentGetter"]),
   },
   methods: {
-    makeToast() {
-      this.$bvToast.toast('New Appointment created successfully', {
-        title:"Heee!",
-        variant: 'success',
-        toaster: 'b-toaster-top-full',
-        solid: true
-      })
+    makeToast(title, description, variant) {
+      this.$bvToast.toast(description, {
+        title: title,
+        variant: variant,
+        toaster: "b-toaster-top-full",
+        solid: true,
+      });
     },
     onSubmit() {
-      let appointmentData = this.appointmentGetter
-      let vm = this
+      let appointmentData = this.appointmentGetter;
+      let vm = this;
       this.base("Appointments").create(
         [
           {
             fields: {
-              "Client": this.form.name,
-              "Client Email": this.form.email,
-              "Client Phone": this.form.phone,
-              "Employee": this.form.employee,
+              "Client": appointmentData.name,
+              "Client Email": appointmentData.email,
+              "Client Phone": appointmentData.phone,
+              "Employee": appointmentData.employee,
               "Appointment Address": appointmentData.address,
               "Appointment Date": appointmentData.date,
               "Appointment Time": appointmentData.time,
@@ -110,24 +71,33 @@ export default {
             },
           },
         ],
-        function (err, records) {
+        function (err) {
           if (err) {
             console.error(err);
+            vm.makeToast(
+              "Opss!",
+              "Something went wrong. Please try again.",
+              "danger"
+            );
             return;
           }
-          vm.makeToast('success')
-          records.forEach(function (record) {
-            console.log(record.getId());
-          });
+          vm.makeToast(
+            "Heey!",
+            "New Appointment created successfully",
+            "success"
+          );
         }
       );
     },
-    onReset(event) {
-      event.preventDefault();
+    onReset() {
       this.form.email = "";
       this.form.name = "";
       this.form.phone = "";
       this.form.user = null;
+      this.form.dateTime = {
+        date : '',
+        time : ''
+      }
     },
   },
 };
