c135213d8088abda4db4c388105ab3496f1d7637
Ercan Uzunsakal <ercan@zeo.org>
2021-12-07 20:01:29 +0300

    Update airtable.js

--- src/plugins/airtable.js
+++ src/plugins/airtable.js
@@ -2,6 +2,6 @@ import Airtable from "airtable";
 
 var base = new Airtable({
   apiKey: process.env.VUE_APP_AIRTABLE_API_KEY,
-}).base(process.env.VUE_APP_AIRTABLE_TABLE_ID);
+}).base("app26Q7TL0rEnifQE");
 
 export default base;
