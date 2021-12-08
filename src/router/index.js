6bba8933769f6343090bf040dbb2802d5c87c994
Ercan Uzunsakal <ercan@zeo.org>
2021-12-08 14:28:01 +0300

    add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import CreateAppointment from '../views/CreateAppointment.vue'
+import Appointments from '../views/Appointments.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/CreateAppointment',
+    name: 'CreateAppointment',
+    component: CreateAppointment
+  },
+  {
+    path: '/UpdateAppointment',
+    name: 'UpdateAppointment',
+    component: CreateAppointment
+  },
+  {
+    path: '/Appointments',
+    name: 'Appointments',
+    component: Appointments
   },
 ]
 
