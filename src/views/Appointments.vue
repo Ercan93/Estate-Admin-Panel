77f4821db102f7ee524d406c62eccdc1c7f3c066
Ercan Uzunsakal <ercan@zeo.org>
2021-12-09 15:08:14 +0300

    add date range filter to appointments table

--- src/views/Appointments.vue
+++ src/views/Appointments.vue
@@ -2,6 +2,21 @@
   <div id="appointments">
     <div>
       <h1>Appointments</h1>
+      <div class="col-11 d-flex align-items-center my-4">
+        <p class="mr-3 mb-0 h4">Select Date Range:</p>
+        <vc-date-picker v-model="range" is-range>
+          <template v-slot="{ inputValue, inputEvents }">
+            <div class="flex justify-center items-center">
+              <input :value="inputValue.start" v-on="inputEvents.start" />
+            </div>
+          </template>
+        </vc-date-picker>
+        <button class="ml-3 btn btn-success" @click="setDateRange">Set</button>
+        <button class="ml-3 btn btn-outline-danger" @click="clearDateRange">
+          Clear
+        </button>
+      </div>
+
       <div class="col-11">
         <ag-grid-vue
           style="height: 500px"
@@ -29,6 +44,11 @@ export default {
       records: null,
       gridApi: null,
       columnApi: null,
+      dateFilterParams: null,
+      range: {
+        start: new Date(),
+        end: new Date(),
+      },
     };
   },
   components: {
@@ -38,7 +58,13 @@ export default {
     this.columnDefs = [
       { field: "name", sortable: true, filter: true },
       { field: "address", sortable: true, filter: true },
-      { field: "date", sortable: true },
+      {
+        field: "date",
+        sortable: true,
+        filter: "agDateColumnFilter",
+        filterParams: this.dateFilterParams,
+        suppressMenu: true,
+      },
       { field: "time", sortable: true, filter: true },
       { field: "employee", sortable: true, filter: true },
       { field: "email", sortable: true, filter: true },
@@ -48,10 +74,60 @@ export default {
     ];
     this.setRowData();
   },
+  created() {
+    this.dateFilterParams = {
+      comparator: function (filterLocalDateAtMidnight, cellValue) {
+        var dateAsString = cellValue;
+        if (dateAsString == null) return -1;
+        var dateParts = dateAsString.split("/");
+        var cellDate = new Date(
+          Number(dateParts[2]),
+          Number(dateParts[1]) - 1,
+          Number(dateParts[0])
+        );
+        if (cellDate >= filterLocalDateAtMidnight) {
+          return 1;
+        }
+        if (cellDate < filterLocalDateAtMidnight) {
+          return -1;
+        }
+      },
+      browserDatePicker: true,
+    };
+  },
   methods: {
     onGridReady(params) {
       this.gridApi = params.api;
       this.gridColumnApi = params.columnApi;
+    },
+    clearDateRange() {
+      this.gridApi.setFilterModel(null);
+    },
+    setDateRange() {
+      let range = this.range;
+      let endDate = `${range.end.getFullYear()}-${
+        range.end.getMonth() + 1
+      }-${range.end.getDate()}`;
+
+      let startDate = `${range.start.getFullYear()}-${
+        range.start.getMonth() + 1
+      }-${range.start.getDate()}`;
+
+      var dateFilterComponent = this.gridApi.getFilterInstance("date");
+      dateFilterComponent.setModel({
+        condition1: {
+          type: "lessThan",
+          dateFrom: endDate,
+          dateTo: null,
+        },
+        operator: "AND",
+        condition2: {
+          type: "greaterThan",
+          dateFrom: startDate,
+          dateTo: null,
+        },
+      });
+      this.gridApi.onFilterChanged();
     },
     setRowData() {
       let vm = this;
