7f74063d41b19fcfb02bd99a704ee102457b311d
Ercan Uzunsakal <ercan@zeo.org>
2021-12-12 17:38:03 +0300

    add custom scrollbar

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,16 @@ export default {
 }
 </script>
 <style>
+::-webkit-scrollbar {
+  width: 10px;
+}
+::-webkit-scrollbar-track {
+  background: #e0f3fc;
+}
+::-webkit-scrollbar-thumb {
+  background: #898c8f;
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #55595c;
+}
 </style>
