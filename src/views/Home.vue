7abaedcb789ef90a92b004d52efd32d16f102f86
Ercan Uzunsakal <ercan@zeo.org>
2021-12-13 16:03:06 +0300

    add jumbotron to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="home">
+    <div class="container d-flex flex-column mt-5 pt-5">
+      <div>
+        <b-jumbotron bg-variant="info" text-variant="white" lead="Approachable, Versatile, Performant">
+          <p class="display-4 font-weight-bold text-white">Estate Panel</p>
+        </b-jumbotron>
+      </div>
+    </div>
   </div>
 </template>
 
