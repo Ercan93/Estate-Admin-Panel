cf70a2d37ad9c9a3062d9c6eace74e88bb3846c6
Ercan Uzunsakal <ercan@zeo.org>
2021-12-13 16:41:59 +0300

    remove records store actions

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    records: null,
     appointment: {
       id: '',
       name: '',
@@ -27,26 +26,17 @@ export default new Vuex.Store({
     },
     appointmentGetter (state) {
       return state.appointment
-    },
-    recordGetter: (state) => (id) => {
-      return state.records.find(record => record.id === id)
     }
   },
   mutations: {
     SET_APPOINTMENT (state, payload) {
       payload = Object.assign(state.appointment, payload)
       state.appointment = payload
-    },
-    SET_RECORDS (state, payload) {
-      state.records = payload
     }
   },
   actions: {
     setAppointment ({ commit }, payload) {
       commit('SET_APPOINTMENT', payload)
-    },
-    setRecords ({ commit }, payload) {
-      commit('SET_RECORDS', payload)
     }
   },
   modules: {}
